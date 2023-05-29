e768fc7c5c44b9358bd788b511cf6b3d4925a22c
Yaroslav Darashkevich <f4stfurios@yandex.ru>
2023-05-29 19:09:26 +1200

    Tree Node - Remove Child 1

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -10,22 +10,38 @@ public class TreeNode {
         this.children = new ArrayList<TreeNode>();
     }
 
-    // addChild() method with parameter TreeNode child
-    public void addChild(TreeNode child){
+    public void addChild(TreeNode child) {
         this.children.add(child);
     }
-    // addChild() method with parameter Object data
-    public void addChild(Object childData){
-        this.children.add(new TreeNode(childData));
+
+    public void addChild(Object childData) {
+        TreeNode child = new TreeNode(childData);
+        this.children.add(child);
     }
+
+    // removeChild() with TreeNode parameter
+    public void removeChild(TreeNode childToRemove){
+        if(this.children.isEmpty()){
+            return ;
+        }
+        else if(this.children.contains(childToRemove)){
+            this.children.remove(childToRemove);
+            return ;
+        }
+        else{
+            for(TreeNode child : this.children){
+                child.removeChild(childToRemove);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(20);
-        // Add child below
-        root.addChild(15);
+        TreeNode root = new TreeNode(30);
+        TreeNode child = new TreeNode(26);
+        root.addChild(child);
+        // Remove child from root
+
         // Print size of root's children list
-        System.out.println(root.children.size());
-        TreeNode biggerChild = new TreeNode(30);
-        root.addChild(biggerChild);
         System.out.println(root.children.size());
     }
 }
