ee0dc68214fa194118413d84208443aa45df5d91
Yaroslav Darashkevich <f4stfurios@yandex.ru>
2023-05-30 09:47:07 +1200

    Binary Search Trees - Inserting A Value

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -5,26 +5,49 @@ public class BinarySearchTree {
     public BinarySearchTree left;
     public BinarySearchTree right;
 
-    public BinarySearchTree(int value, int depth){
+    public BinarySearchTree(int value, int depth) {
         this.value = value;
         this.depth = depth;
         this.left = null;
         this.right = null;
     }
 
-    public BinarySearchTree(int value){
+    public BinarySearchTree(int value) {
         this.value = value;
         this.depth = 1;
         this.left = null;
         this.right = null;
     }
 
+    // Define insert() below
+    public void insert(int value){
+        if(value < this.value){
+            if(this.left == null){
+                this.left = new BinarySearchTree(value, this.depth + 1);
+                System.out.printf("Tree node %d added to the left of %d at depth %d \n", value, this.value, this.depth + 1);
+            }
+            else{
+                this.left.insert(value);
+            }
+        }
+        else{
+            if(this.right == null){
+                this.right = new BinarySearchTree(value, this.depth + 1);
+                System.out.printf("Tree node %d added to the right of %d at depth %d \n", value, this.value, this.depth + 1);
+            }
+            else{
+                this.right.insert(value);
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        // Create a new BST:
-        BinarySearchTree bt = new BinarySearchTree(15);
+        BinarySearchTree root = new BinarySearchTree(100);
 
-        // Print bt's value below:
-        System.out.println(bt.value);
+        // Insert values below:
+        root.insert(50);
+        root.insert(125);
+        root.insert(75);
+        root.insert(25);
     }
 }
