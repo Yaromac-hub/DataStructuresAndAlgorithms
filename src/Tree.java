b938dbcfdedef6701a92cee8e1ae74e045f95738
Yaroslav Darashkevich <f4stfurios@yandex.ru>
2023-05-29 19:29:31 +1200

    Tree Node - Depth First Traversal

--- src/Tree.java
+++ src/Tree.java
@@ -20,28 +20,30 @@ public class Tree {
         }
     }
 
+    // Define depthFirstTraversal() below
+    public void depthFirstTraversal(TreeNode current){
+        System.out.print(current.data + " ");
+        for(TreeNode child : current.children){
+            depthFirstTraversal(child);
+        }
+    }
+
+
     public static void main(String[] args) {
         TreeNode treeRoot = new TreeNode("S");
         TreeNode child1 = new TreeNode("N");
         TreeNode child2 = new TreeNode("O");
         TreeNode grandchild1 = new TreeNode("W");
         TreeNode grandchild2 = new TreeNode("Y");
-        // addChild operations below
         treeRoot.addChild(child1);
         treeRoot.addChild(child2);
         child1.addChild(grandchild1);
         child2.addChild(grandchild2);
-        // Create tree
+
         Tree tree = new Tree(treeRoot);
-        // Print tree
         tree.print();
-        // Remove grandchild2 from the tree
-        treeRoot.removeChild(grandchild2);
-        // Print the tree
-        tree.print();
-        // Remove "O" from the tree
-        treeRoot.removeChild("O");
-        // Print the tree again
-        tree.print();
+
+        // Call depthFirstTraversal below
+        tree.depthFirstTraversal(treeRoot);
     }
 }
