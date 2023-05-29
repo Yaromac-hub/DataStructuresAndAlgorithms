85be1244c72a6e5e3ed63f9bc5faa33f6ca2dadf
Yaroslav Darashkevich <f4stfurios@yandex.ru>
2023-05-30 10:00:42 +1200

    Binary Search Trees - Retrieving A Node By Value

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -19,35 +19,47 @@ public class BinarySearchTree {
         this.right = null;
     }
 
-    // Define insert() below
-    public void insert(int value){
-        if(value < this.value){
-            if(this.left == null){
+    public void insert(int value) {
+        if (value < this.value) {
+            if (this.left == null) {
                 this.left = new BinarySearchTree(value, this.depth + 1);
-                System.out.printf("Tree node %d added to the left of %d at depth %d \n", value, this.value, this.depth + 1);
-            }
-            else{
+                System.out.printf("Tree node %d added to the %s of %d at depth %d \n", value, "left", this.value, this.depth + 1);
+            } else {
                 this.left.insert(value);
             }
-        }
-        else{
-            if(this.right == null){
+        } else {
+            if (this.right == null) {
                 this.right = new BinarySearchTree(value, this.depth + 1);
-                System.out.printf("Tree node %d added to the right of %d at depth %d \n", value, this.value, this.depth + 1);
-            }
-            else{
+                System.out.printf("Tree node %d added to the %s of %d at depth %d \n", value, "right", this.value, this.depth + 1);
+            } else {
                 this.right.insert(value);
             }
         }
     }
 
+    // Define getNodeByValue() below
+    public BinarySearchTree getNodeByValue(int value){
+        if(this.value == value){
+            return this;
+        }
+        else if(value < this.value && this.left != null){
+            return this.left.getNodeByValue(value);
+        }
+        else if(value > this.value && this.right != null){
+            return this.right.getNodeByValue(value);
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         BinarySearchTree root = new BinarySearchTree(100);
-
-        // Insert values below:
         root.insert(50);
         root.insert(125);
         root.insert(75);
         root.insert(25);
+        // Get nodes by value below
+        System.out.println(root.getNodeByValue(75));
+        System.out.println(root.getNodeByValue(55));
+
     }
 }
