fb9b4c0ec62a4b6d5e75603b8aa7f3a5c019d061
Yaroslav Darashkevich <f4stfurios@yandex.ru>
2023-06-09 11:03:58 +1200

    Adding an Element: Bubble Up Pt 1

--- src/MinHeap.java
+++ src/MinHeap.java
@@ -1,21 +1,35 @@
 import java.util.ArrayList;
 public class MinHeap {
-
     public ArrayList<Integer> heap;
     public int size;
 
-    // Define MinHeap constructor below
-    public  MinHeap () {
+    public MinHeap() {
         this.heap = new ArrayList<Integer>();
         this.heap.add(null);
         this.size = 0;
     }
 
-    public static void main(String[]args) {
-        // Instantiate a MinHeap, assign to minHeap
-        MinHeap minHeap = new MinHeap();
-        // Display contents of minHeap
-        System.out.println(minHeap.heap);
+    // Define add() below
+    public void add(int value){
+        this.heap.add(value);
+        this.size++;
+        System.out.println("Adding " + value);
+        System.out.println(this.heap);
+        this.bubbleUp();
     }
 
+
+    // Define bubbleUp() below
+    private void bubbleUp(){
+        System.out.println("Restoring heap condition...");
+    }
+
+
+    public static void main(String[]args) {
+        MinHeap minHeap = new MinHeap();
+        System.out.println(minHeap.heap);
+
+        // Add 42 to minHeap using add() below
+        minHeap.add(42);
+    }
 }
