9d3cecc84818099d548fda19291ae120ac4a5e28
Yaroslav Darashkevich <f4stfurios@yandex.ru>
2023-06-12 15:18:02 +1200

    MERGE SORT - Splitting: Recursive Case

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -4,15 +4,22 @@ public class MergeSort {
 
     public int[] sort(int[] arr) {
         int length = arr.length;
-        if(length<=1){
+        if (length <= 1) {
+            System.out.println(Arrays.toString(arr));
             return arr;
         }
+        int mid = Math.floorDiv(length, 2);
+        int[] leftArray = Arrays.copyOfRange(arr, 0, mid);
+        int[] rightArray = Arrays.copyOfRange(arr, mid, length);
+        sort(leftArray);
+        sort(rightArray);
         return null;
     }
 
     public static void main (String[] args) {
-        int[] inputArr = {3};
+
+        int[] inputArr = {3, 5, 2, 90, 4, 7};
         MergeSort sorter = new MergeSort();
-        System.out.println(Arrays.toString(sorter.sort(inputArr)));
+        sorter.sort(inputArr);
     }
 }
