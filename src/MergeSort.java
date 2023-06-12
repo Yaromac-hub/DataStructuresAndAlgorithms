39928b13a1441c691fdd50d0d34322a9cf1e07e7
Yaroslav Darashkevich <f4stfurios@yandex.ru>
2023-06-12 15:11:48 +1200

    MERGE SORT - Splitting: Base Case

---
+++ src/MergeSort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public int[] sort(int[] arr) {
+        int length = arr.length;
+        if(length<=1){
+            return arr;
+        }
+        return null;
+    }
+
+    public static void main (String[] args) {
+        int[] inputArr = {3};
+        MergeSort sorter = new MergeSort();
+        System.out.println(Arrays.toString(sorter.sort(inputArr)));
+    }
+}
