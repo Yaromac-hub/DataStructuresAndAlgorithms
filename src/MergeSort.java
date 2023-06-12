b6db23c1891e19a9ba82b9abefbcbcdbd2a4d3fd
Yaroslav Darashkevich <f4stfurios@yandex.ru>
2023-06-12 15:23:19 +1200

    MERGE SORT - Call .merge() from .sort()

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -5,14 +5,17 @@ public class MergeSort {
     public int[] sort(int[] arr) {
         int length = arr.length;
         if (length <= 1) {
-            System.out.println(Arrays.toString(arr));
             return arr;
         }
         int mid = Math.floorDiv(length, 2);
         int[] leftArray = Arrays.copyOfRange(arr, 0, mid);
         int[] rightArray = Arrays.copyOfRange(arr, mid, length);
-        sort(leftArray);
-        sort(rightArray);
+        return merge(sort(leftArray), sort(rightArray));
+    }
+
+    public int[] merge(int left[], int[] right) {
+        System.out.println(Arrays.toString(left));
+        System.out.println(Arrays.toString(right));
         return null;
     }
 
@@ -20,6 +23,7 @@ public class MergeSort {
 
         int[] inputArr = {3, 5, 2, 90, 4, 7};
         MergeSort sorter = new MergeSort();
-        sorter.sort(inputArr);
+        System.out.println(Arrays.toString(sorter.sort(inputArr)));
+
     }
 }
